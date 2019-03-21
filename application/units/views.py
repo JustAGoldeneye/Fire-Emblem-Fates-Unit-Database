56b114251cdddca8ca8712d8f96b2fd4f18f7d11
JustAGoldeneye <eemeli.leppa@gmail.com>
2019-03-21 19:16:42 +0200

    Stats have now a min value of 0 (1 for level and hp)

--- application/units/views.py
+++ application/units/views.py
@@ -23,7 +23,8 @@ def units_delete(unit_id):
 def units_create():
     u = Unit(request.form.get('name'), request.form.get('classGP'), request.form.get('level'), request.form.get('hp'), request.form.get('strength'), request.form.get('magic'), request.form.get('skill'), request.form.get('speed'), request.form.get('luck'), request.form.get('defense'), request.form.get('resistance'), request.form.get('movement'), request.form.get('hpGrowth'), request.form.get('strengthGrowth'), request.form.get('magicGrowth'), request.form.get('skillGrowth'), request.form.get('speedGrowth'), request.form.get('luckGrowth'), request.form.get('defenseGrowth'), request.form.get('resistanceGrowth'))
 
-    db.session().add(u)
-    db.session().commit()
+    if int(u.level) >= 1 and int(u.hp) >= 1 and int(u.strength) >= 0 and int(u.magic) >= 0 and int(u.skill) >= 0 and int(u.speed) >= 0 and int(u.luck) >= 0 and int(u.defense) >= 0 and int(u.resistance) >= 0 and int(u.movement) >= 0:
+        db.session().add(u)
+        db.session().commit()
 
     return redirect(url_for("units_index"))
