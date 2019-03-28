c22f1d5d240041511883523934dd7869f3bbdba5
JustAGoldeneye <eemeli.leppa@gmail.com>
2019-03-28 19:58:49 +0200

    Errors messages add for Add a unit

--- application/units/forms.py
+++ application/units/forms.py
@@ -1,27 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, validators
 
 class UnitForm(FlaskForm):
-    name = StringField("Name")
-    classGP = StringField("Class")
-    level = IntegerField("Level")
-    hp = IntegerField("HP")
-    strength = IntegerField("Strength")
-    magic = IntegerField("Magic")
-    skill = IntegerField("Skill")
-    speed = IntegerField("Speed")
-    luck = IntegerField("Luck")
-    defense = IntegerField("Defense")
-    resistance = IntegerField("Resistance")
-    movement = IntegerField("Movement")
-    hpGrowth = IntegerField("HP Growth %")
-    strengthGrowth = IntegerField("Strength Growth %")
-    magicGrowth = IntegerField("Magic Growth %")
-    skillGrowth = IntegerField("Skill Growth %")
-    speedGrowth = IntegerField("Speed Growth %")
-    luckGrowth = IntegerField("Luck Growth %")
-    defenseGrowth = IntegerField("Defense Growth %")
-    resistanceGrowth = IntegerField("Resistance Growth %")
+    name = StringField("Name", [validators.Length(min=1)])
+    classGP = StringField("Class", [validators.Length(min=1)])
+    level = IntegerField("Level", [validators.NumberRange(min=1)])
+    hp = IntegerField("HP", [validators.NumberRange(min=1)])
+    strength = IntegerField("Strength", [validators.NumberRange(min=0)])
+    magic = IntegerField("Magic", [validators.NumberRange(min=0)])
+    skill = IntegerField("Skill", [validators.NumberRange(min=0)])
+    speed = IntegerField("Speed", [validators.NumberRange(min=0)])
+    luck = IntegerField("Luck", [validators.NumberRange(min=0)])
+    defense = IntegerField("Defense", [validators.NumberRange(min=0)])
+    resistance = IntegerField("Resistance", [validators.NumberRange(min=0)])
+    movement = IntegerField("Movement", [validators.NumberRange(min=0)])
+    hpGrowth = IntegerField("HP Growth %", [validators.NumberRange(min=0)])
+    strengthGrowth = IntegerField("Strength Growth %", [validators.NumberRange(min=0)])
+    magicGrowth = IntegerField("Magic Growth %", [validators.NumberRange(min=0)])
+    skillGrowth = IntegerField("Skill Growth %", [validators.NumberRange(min=0)])
+    speedGrowth = IntegerField("Speed Growth %", [validators.NumberRange(min=0)])
+    luckGrowth = IntegerField("Luck Growth %", [validators.NumberRange(min=0)])
+    defenseGrowth = IntegerField("Defense Growth %", [validators.NumberRange(min=0)])
+    resistanceGrowth = IntegerField("Resistance Growth %", [validators.NumberRange(min=0)])
 
     class Meta:
         csrf = False
--- application/units/views.py
+++ application/units/views.py
@@ -24,10 +24,12 @@ def units_delete(unit_id):
 def units_create():
     form = UnitForm(request.form)
 
+    if not form.validate():
+        return render_template("units/new.html", form = form)
+
     u = Unit(form.name.data, form.classGP.data, form.level.data, form.hp.data, form.strength.data, form.magic.data, form.skill.data, form.speed.data, form.luck.data, form.defense.data, form.resistance.data, form.movement.data, form.hpGrowth.data, form.strengthGrowth.data, form.magicGrowth.data, form.skillGrowth.data, form.speedGrowth.data, form.luckGrowth.data, form.defenseGrowth.data, form.resistanceGrowth.data)
 
-    if int(u.level) >= 1 and int(u.hp) >= 1 and int(u.strength) >= 0 and int(u.magic) >= 0 and int(u.skill) >= 0 and int(u.speed) >= 0 and int(u.luck) >= 0 and int(u.defense) >= 0 and int(u.resistance) >= 0 and int(u.movement) >= 0:
-        db.session().add(u)
-        db.session().commit()
+    db.session().add(u)
+    db.session().commit()
 
     return redirect(url_for("units_index"))
